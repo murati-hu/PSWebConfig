5eba741e132c138a444c39ba4dc6c3c23d8a2f89
Akos Murati <akos@murati.hu>
2016-02-19 16:54:34 +1300

    Add more format tests

--- Tests/Get-WebConfig.tests.ps1
+++ Tests/Get-WebConfig.tests.ps1
@@ -2,8 +2,9 @@
 
 $TestVerbose=$false
 
-#region Test-WebConfig
-$webConfigFile = Join-Path $PSScriptRoot 'ConfigTests/web.config'
+$webConfigFolder = Join-Path $PSScriptRoot 'ConfigTests'
+$webConfigFile = Join-Path $webConfigFolder 'web.config'
+
 $webConfigSections = @(
     "appSettings"
     "connectionStrings"
@@ -35,21 +36,60 @@ Describe "Get-PSWebConfig" {
         }
     }
 
-    Context "Local file" {
-        # Load test file
-        $config = Get-PSWebConfig -Path $webConfigFile -Verbose:$TestVerbose
+    Context "Default XML output" {
+        It "should return the XML object by default" {
+            $defaultConfig = Get-PSWebConfig -Path $webConfigFile -Verbose:$TestVerbose
+            $defaultConfig | Should Not BeNullOrEmpty
+            $defaultConfig.GetType().Name | Should Be "XmlDocument"
+            #$defaultConfig.OuterXml | Set-Content ./default.txt -Enc UTF8
 
-        It "Should be able to read the file" {
+            $xmlConfig = Get-PSWebConfig -Path $webConfigFile -AsXml -Verbose:$TestVerbose
+            $xmlConfig | Should Not BeNullOrEmpty
+            $xmlConfig.GetType().Name | Should Be "XmlDocument"
+            #$xmlConfig.OuterXml | Set-Content ./xml.txt -Enc UTF8
+
+            $defaultConfig.OuterXml | Should Be $xmlConfig.OuterXml
+        }
+
+        It "should have PSWebConfig.WebConfig additional type" {
+            $config = Get-PSWebConfig -Path $webConfigFile -Verbose:$TestVerbose
             $config | Should Not BeNullOrEmpty
+            $config.psobject.TypeNames -contains 'PsWebConfig.WebConfig' | Should Be $true
         }
 
-        It "Should be a valid XMLDocument" {
+        It "Should be a valid XML Configuration" {
+            $config = Get-PSWebConfig -Path $webConfigFile -Verbose:$TestVerbose
             $config.GetType().Name | Should Be "XmlDocument"
-        }
-
-        It "Should have a configuration section" {
             $config.configuration | Should Not BeNullOrEmpty
             $config.configuration.GetType().Name | Should Be "XmlElement"
+        }
+    }
+
+    Context "FileName output" {
+        It "should match the source filename as a string" {
+            $config = Get-PSWebConfig -Path $webConfigFile -AsFileName -Verbose:$TestVerbose
+            $config | Should Not BeNullOrEmpty
+            $config | Should Be $webConfigFile
+            $config.GetType().Name | Should Be 'String'
+        }
+
+        It "should find web.config in folders" {
+            $config = Get-PSWebConfig -Path $webConfigFolder -AsFileName -Verbose:$TestVerbose
+            $config | Should Not BeNullOrEmpty
+            $config | Should Be $webConfigFile
+        }
+    }
+
+    Context "Unencrypted text output" {
+        It "should be the same XML string as the source file" {
+            $config = Get-PSWebConfig -Path $webConfigFile -AsText -Verbose:$TestVerbose
+            $config | Should Not BeNullOrEmpty
+            $config.GetType().Name | Should Be 'String'
+
+            $rawConfig = Get-Content -Path $webConfigFile
+
+            #Convert both String to XML to strip unnessary whitespaces
+            ([xml]$config).OuterXml  | Should Be ([xml]$rawConfig).OuterXml
         }
     }
 }
