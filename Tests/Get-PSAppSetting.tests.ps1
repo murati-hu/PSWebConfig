ac75a968fa8bf34826fc3c79350bb5e6164d3982
Akos Murati <akos@murati.hu>
2016-02-22 22:36:17 +1300

    Add PSAppSetting test

---
+++ Tests/Get-PSAppSetting.tests.ps1
@@ -0,0 +1,24 @@
+. (Join-Path $PSScriptRoot Import-LocalModule.ps1)
+
+$TestVerbose=$false
+
+$webConfigFolder = Join-Path $PSScriptRoot 'ConfigTests'
+$webConfigFile = Join-Path $webConfigFolder 'web.config'
+
+Describe "Get-PSAppSetting" {
+    Context "Local web.config" {
+        $config = Get-PSWebConfig -Path $webConfigFile -Verbose:$TestVerbose
+        $appSettings = $config | Get-PSAppSetting -Verbose:$TestVerbose
+
+        It "should return all AppSettings" {
+            $appSettings | Should Not BeNullOrEmpty
+            $appSettings.Count | Should Be 5
+            $connStrs | Foreach-Object {
+                $_.psobject.TypeNames -contains 'PSWebConfig.AppSetting' | Should Be $true
+                $_.SectionPath | Should Be 'appSettings'
+                $_.key  | Should Not BeNullOrEmpty
+                $_.value  | Should  Not BeNullOrEmpty
+            }
+        }
+    }
+}
